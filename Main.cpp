6acf74cc646639480c5ad0abd606aa1d9fb46653
EffeX <48663440+effex1337@users.noreply.github.com>
2020-10-06 10:18:56 +0545

    Cleanup

--- Main.cpp
+++ Main.cpp
@@ -1,39 +1,6 @@
 #include "Main.h"
 
-int main(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-	cout << "Welcome to EffeX's Multi Loader!";
-	::ShowWindow(::GetConsoleWindow(), SW_SHOW);// show console
-	Sleep(2000);
-	::ShowWindow(::GetConsoleWindow(), SW_HIDE);// hide console
-	LPCTSTR lpzClass = NAME;
-	if (!RegMyWindowClass(hInstance, lpzClass))
-		return 1;
-	RECT screen_rect;
-	GetWindowRect(GetDesktopWindow(), &screen_rect);
-	int x = screen_rect.right / 2 - 150;
-	int y = screen_rect.bottom / 2 - 75;
-	HWND hWnd = CreateWindow(lpzClass, NAME, WS_POPUP, x, y, WINDOW_WIDTH, WINDOW_HEIGHT, NULL, NULL, hInstance, NULL);
-	if (!hWnd) return 2;
-	LPDIRECT3D9 pD3D;
-	if ((pD3D = Direct3DCreate9(D3D_SDK_VERSION)) == NULL) {
-		UnregisterClass(lpzClass, hInstance);
-	}
-
-	ZeroMemory(&g_d3dpp, sizeof(g_d3dpp));
-	g_d3dpp.Windowed = TRUE;
-	g_d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
-	g_d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
-	g_d3dpp.EnableAutoDepthStencil = TRUE;
-	g_d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
-	g_d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_ONE;
-	if (pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &g_d3dpp, &g_pd3dDevice) < 0) {
-		pD3D->Release();
-		UnregisterClass(lpzClass, hInstance);
-		return 0;
-	}
-
-	ImGui_ImplDX9_Init(hWnd, g_pd3dDevice);
-
+void update_colors() {
 	ImGuiStyle* style = &ImGui::GetStyle();
 	style->WindowPadding = ImVec2(15, 15);
 	style->WindowRounding = 5.0f;
@@ -92,11 +59,56 @@ int main(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmd
 	style->Colors[ImGuiCol_PlotHistogramHovered] = ImVec4(0.25f, 1.00f, 0.00f, 1.00f);
 	style->Colors[ImGuiCol_TextSelectedBg] = ImVec4(0.25f, 1.00f, 0.00f, 0.43f);
 	style->Colors[ImGuiCol_ModalWindowDarkening] = ImVec4(1.00f, 0.98f, 0.95f, 0.73f);
+}
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow){
+	LPCTSTR lpzClass = LOADER_BRAND;
+	if (!RegMyWindowClass(hInstance, lpzClass)) {
+		return 1;
+	}
+
+	//center the loader
+	RECT screen_rect;
+	GetWindowRect(GetDesktopWindow(), &screen_rect);
+	int x = (screen_rect.right - WINDOW_WIDTH) / 2;
+	int y = (screen_rect.bottom - WINDOW_HEIGHT) / 2;
+
+	//window creation
+	hWnd = CreateWindow(lpzClass, LOADER_BRAND, WS_POPUP, x, y, WINDOW_WIDTH, WINDOW_HEIGHT, NULL, NULL, hInstance, NULL);
+	if (!hWnd) {
+		MessageBox(0, "Error", "Window Creation Failed", 0);
+		return 0;
+	}
+
+	//initialize d3d
+	LPDIRECT3D9 pD3D;
+	if ((pD3D = Direct3DCreate9(D3D_SDK_VERSION)) == NULL) {
+		UnregisterClass(lpzClass, hInstance);
+	}
+
+	ZeroMemory(&g_d3dpp, sizeof(g_d3dpp));
+	g_d3dpp.Windowed = TRUE;
+	g_d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	g_d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
+	g_d3dpp.EnableAutoDepthStencil = TRUE;
+	g_d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
+	g_d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_ONE;
+
+	if (pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &g_d3dpp, &g_pd3dDevice) < 0) {
+		pD3D->Release();
+		UnregisterClass(lpzClass, hInstance);
+		return 0;
+	}
+
+	ImGui_ImplDX9_Init(hWnd, g_pd3dDevice);
 
 	MSG msg;
 	ZeroMemory(&msg, sizeof(msg));
+
 	ShowWindow(hWnd, SW_SHOWDEFAULT);
 	UpdateWindow(hWnd);
+
+	//main loop
 	while (msg.message != WM_QUIT)
 	{
 		if (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
@@ -108,14 +120,14 @@ int main(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmd
 
 		ImGui_ImplDX9_NewFrame();
 
-		DWORD dwFlag = ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse;
+		DWORD window_flags = ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoTitleBar;
 
-		static bool open = true;
+		if (!loader_active) ExitProcess(0);
 
-		if (!open) ExitProcess(0);
-
-		ImGui::Begin(NAME_LOADER, &open, ImVec2(WINDOW_WIDTH, WINDOW_HEIGHT), 1.0f, dwFlag); {
-
+		update_colors();
+		ImGui::Begin(LOADER_BRAND, &loader_active, ImVec2(WINDOW_WIDTH, WINDOW_HEIGHT), 1.0f, window_flags);
+		{
+			ImGui::Text(LOADER_BRAND);
 		}
 		ImGui::End();
 
@@ -129,9 +141,49 @@ int main(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmd
 		}
 		g_pd3dDevice->Present(NULL, NULL, NULL, NULL);
 	}
+
+	//clean up
 	ImGui_ImplDX9_Shutdown();
 	if (g_pd3dDevice) g_pd3dDevice->Release();
 	if (pD3D) pD3D->Release();
-	UnregisterClass(NAME, hInstance);
+	UnregisterClass(LOADER_BRAND, hInstance);
 	return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+	if (ImGui_ImplDX9_WndProcHandler(hWnd, message, wParam, lParam))
+		return true;
+
+	switch (message) {
+	case WM_SIZE:
+		if (g_pd3dDevice != NULL && wParam != SIZE_MINIMIZED)
+		{
+			ImGui_ImplDX9_InvalidateDeviceObjects();
+			g_d3dpp.BackBufferWidth = LOWORD(lParam);
+			g_d3dpp.BackBufferHeight = HIWORD(lParam);
+			HRESULT hr = g_pd3dDevice->Reset(&g_d3dpp);
+			if (hr == D3DERR_INVALIDCALL) IM_ASSERT(0);
+			ImGui_ImplDX9_CreateDeviceObjects();
+		}
+		return 0;
+
+	case WM_SYSCOMMAND:
+		if ((wParam & 0xfff0) == SC_KEYMENU) return 0;
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+ATOM RegMyWindowClass(HINSTANCE hInst, LPCTSTR lpzClassName) {
+	WNDCLASS wcWindowClass = { 0 };
+	wcWindowClass.lpfnWndProc = (WNDPROC)WndProc;
+	wcWindowClass.style = CS_HREDRAW | CS_VREDRAW;
+	wcWindowClass.hInstance = hInst;
+	wcWindowClass.lpszClassName = lpzClassName;
+	wcWindowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcWindowClass.hbrBackground = (HBRUSH)COLOR_APPWORKSPACE;
+	return RegisterClass(&wcWindowClass);
 }
